34414a66a804eda47772d14b5482eb9998c168b1
Luciano Morais <lucianopythonista@gmail.com>
2022-04-03 09:54:22 -0300

    Configuracao dos modulos da aplicacao

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DataBaseModule } from 'config/database/database.module';
+import { UserModule } from './user/user.module';
+import { UserController } from './user/users.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    DataBaseModule,
+    UserModule
+  ],
+  controllers: [
+    UserController
+  ]
 })
 export class AppModule {}
