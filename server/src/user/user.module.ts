234eacc06c69e718a625112f4579c8fa01749816
Luciano Morais <lucianopythonista@gmail.com>
2022-04-06 02:30:23 -0300

    Correção nos módulos

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { DataBaseModule } from "../../config/database/database.module";
 import { UserService } from "src/user/user.service";
 import { userProviders } from "./user.providers";
+import { UserController } from "./users.controller";
 
 @Module({
     imports: [DataBaseModule],
+    controllers: [UserController],
     providers: [UserService, ...userProviders],
     exports: [UserService]
 })
