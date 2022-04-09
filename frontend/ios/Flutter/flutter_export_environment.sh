9e4ddb26ada9e913d17b94b498c3dabe985baeee
brunojviana <brunojviana@hotmail.com>
2022-04-09 18:29:11 -0300

    inclusão do frontend na main

---
+++ frontend/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/bruno/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/bruno/Documentos/IESB/6Semestre/PDM/GerenciaStreamingApp/frontend"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
