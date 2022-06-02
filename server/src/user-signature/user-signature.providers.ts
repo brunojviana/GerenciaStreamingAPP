5a5b1674d284b51d2726d5362dea5037f240136f
Luciano Morais <lucianopythonista@gmail.com>
2022-06-02 20:57:54 -0300

    Remoção do módulo de relacionamento n:n

--- server/src/user-signature/user-signature.providers.ts
+++
@@ -1,9 +0,0 @@
-import { USER_SIGNATURE_REPOSITORY } from "./constants";
-import { User_Signature } from "./user-signature.model";
-
-export const user_signatureProviders = [
-    {
-        provide: USER_SIGNATURE_REPOSITORY,
-        useValue: User_Signature
-    }
-];
